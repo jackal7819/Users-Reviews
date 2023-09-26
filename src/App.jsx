4385e7bdbc9b2d6cb6004a323c8d6101b58cd372
Viktor Filippov <jackal3166@gmail.com>
2023-09-26 07:57:06 +0300

    Add Random

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { people } from './data';
 import { useState } from 'react';
 
 const App = () => {
     const [index, setIndex] = useState(0);
     const { name, job, image, text } = people[index];
-    return <h2>{name}</h2>;
+
+    const handleNext = () => {
+        setIndex((prevIndex) => (prevIndex < 3 ? prevIndex + 1 : prevIndex));
+    };
+
+    const handlePrev = () => {
+        setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+    };
+
+    const handleRandom = () => {
+        let newIndex;
+        while (
+            (newIndex = Math.floor(Math.random() * people.length)) === index
+        );
+        setIndex(newIndex);
+    };
+
+    return (
+        <main>
+            <article className='review'>
+                <div className='img-container'>
+                    <img src={image} alt={name} className='person-img' />
+                    <span className='quote-icon'>
+                        <FaQuoteRight />
+                    </span>
+                </div>
+                <h4 className='author'>{name}</h4>
+                <p className='job'>{job}</p>
+                <p className='info'>{text}</p>
+                <div className='btn-container'>
+                    <button
+                        className='prev-btn'
+                        onClick={handlePrev}
+                        disabled={index === 0}>
+                        <FaChevronLeft />
+                    </button>
+                    <button
+                        className='prev-btn'
+                        onClick={handleNext}
+                        disabled={index === people.length - 1}>
+                        <FaChevronRight />
+                    </button>
+                </div>
+                <button className='btn btn-hipster' onClick={handleRandom}>
+                    surprise me
+                </button>
+            </article>
+        </main>
+    );
 };
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 export const people = [
     {
         id: 1,
-        name: 'John Doe',
+		name: 'Alice Johnson',
         job: 'Software Engineer',
         image: 'https://course-api.com/images/people/person-1.jpeg',
-        text: "I've been using the Reviews App for a while now, and it's been a fantastic experience. The user-friendly interface makes it easy to navigate through customer reviews. It's especially handy when I want to read random reviews for a fresh perspective. Highly recommended!",
+        text: "I've been using the Reviews App for a while now, and it's been a fantastic experience. The user-friendly interface makes it easy to navigate. It's especially handy when I want to read random reviews for a fresh perspective.",
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const people = [
     },
     {
         id: 3,
-        name: 'Alice Johnson',
+        name: 'John Doe',
         job: 'Product Manager',
         image: 'https://course-api.com/images/people/person-4.jpeg',
         text: "As a Product Manager, I rely on the Reviews App to stay updated with customer sentiments. The app's simple yet effective design allows me to dive deep into individual reviews. It's an invaluable tool for me and my team!",
@@ -25,6 +25,6 @@ export const people = [
         name: 'David Anderson',
         job: 'CEO',
         image: 'https://course-api.com/images/people/person-3.jpeg',
-        text: "Our company's success is tied to our customers, and the Reviews App has become a vital part of our decision-making process. It's intuitive, and the random review feature often provides unique insights. Kudos to the team behind this amazing app!",
+        text: "Our company's success is tied to our customers, and the Reviews App has become a vital part of our decision-making process. It's intuitive, and the review feature often provides unique insights. Kudos to the team behind this amazing app!",
     },
 ];
